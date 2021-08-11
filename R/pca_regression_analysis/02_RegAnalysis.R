0b1537b9b050685a48813eb851eeb56178062062
luizpedrocouto <65123020+luizpedrocouto@users.noreply.github.com>
2021-08-11 15:53:29 -0300

    Add files via upload

--- R/pca_regression_analysis/02_RegAnalysis.R
+++ R/pca_regression_analysis/02_RegAnalysis.R
@@ -11,6 +11,7 @@ library('car')
 library('ggcorrplot')
 library('doParallel')
 library('normtest')
+library('VGAM')
 
 #LOAD DATA BASE
 pca_regression_df_ready_to_use <- readRDS(
@@ -42,54 +43,28 @@ plot_qq(onlynumbersbase)
 
 normtest::kurtosis.norm.test(onlynumbersbase,nrepl = 1000)
 
-## logaritmizing ASSIMETRIC VARIABLES (base on qqplot)
+## Data Transforming
 
-lnreghib <- onlynumbersbase %>% dplyr::mutate(
-  x_avg_cell_distance=log(onlynumbersbase$x_avg_cell_distance),
-  x_avg_cell_distane_w_pop=log(onlynumbersbase$x_avg_cell_distance_w_pop),
-  x_mean_slope=log(onlynumbersbase$x_mean_slope),
-  x_intersection_density_km=log(onlynumbersbase$x_intersection_density_km),
-x_n_patches=log(onlynumbersbase$x_n_patches),x_circuity_avg=log(onlynumbersbase$x_circuity_avg),
-x_intersection_count=log(onlynumbersbase$x_intersection_count),
-x_density_pop_05km2_consolidada_2014=log(onlynumbersbase$x_density_pop_05km2_consolidada_2014),
-x_dissimilarity_5km=log(onlynumbersbase$x_dissimilarity_5km),
-x_prop_car_motorcycle_pes=log(onlynumbersbase$x_prop_car_motorcycle_pes),
-x_built_total_total_2014=log(onlynumbersbase$x_built_total_total_2014),
-x_built_total_consolidada_2014=log(onlynumbersbase$x_built_total_consolidada_2014),
-x_prop_work_other_muni=log(onlynumbersbase$x_prop_work_other_muni),
-x_prop_formal=log(onlynumbersbase$x_prop_formal),x_prop_dom_urban=log(
-  onlynumbersbase$x_prop_dom_urban),
-x_prop_black_men=log(onlynumbersbase$x_prop_black_men),x_prop_white_women=log(
-  onlynumbersbase$x_prop_white_women),
-x_wghtd_mean_density_resident_household=log(
-  onlynumbersbase$x_wghtd_mean_density_resident_household),
-x_prop_car_motorcycle_dom=log(onlynumbersbase$x_prop_car_motorcycle_dom),
-x_pop_2015=log(onlynumbersbase$x_pop_2015),
-x_wghtd_mean_density_resident_bedroom=log(onlynumbersbase$x_wghtd_mean_density_resident_bedroom),
-y_fuel_consumption_per_capita_2010=log(onlynumbersbase$y_fuel_consumption_per_capita_2010),
-y_wghtd_mean_commute_time=log(onlynumbersbase$y_wghtd_mean_commute_time))
+basenumberyeo <- VGAM::yeo.johnson(onlynumbersbase,lambda = )
 
-summary(lnreghib)
-
-plot_intro(lnreghib)
-
+onlynmbrhip <- onlynumbersbase %>%
+  dplyr::mutate_if(is.numeric,~log(((.x + .x^2+1)^(1/2))))
 
 ##########CARET############ ----
 
-###### CARET SAFS CRITERIA APPLICATION
+###### CARET SAFS CRITERIA APPLICATIon
 
-x <- lnreghib %>% dplyr::mutate(y_fuel_consumption_per_capita_2010=NULL,
+x <- onlynmbrhip %>% dplyr::mutate(y_fuel_consumption_per_capita_2010=NULL,
                                 y_wghtd_mean_commute_time=NULL)
 
-y1 <- pca_regression_df_ready_to_use$y_fuel_consumption_per_capita_2010
-y2 <- pca_regression_df_ready_to_use$y_wghtd_mean_commute_time
+y1 <- onlynmbrhip$y_fuel_consumption_per_capita_2010
+y2 <- onlynmbrhip$y_wghtd_mean_commute_time
 
 summary(x)
 DataExplorer::plot_intro(x)
 DataExplorer::plot_histogram(x)
 DataExplorer::plot_missing(x)
 DataExplorer::plot_qq(x)
-DataExplorer::plot_boxplot(x)
 
 # SEGUINDO CRITERIO DO CARET PARA REMOVER VARIÁVEIS COM ALTA COLINEARIDADE
 
@@ -97,7 +72,7 @@ comboInfo <- findLinearCombos(x)
 comboInfo
 ## PARALELIZANDO
 
-cl <- makePSOCKcluster(5)
+cl <- makePSOCKcluster(3)
 registerDoParallel(cl)
 
 ### CARET
@@ -126,7 +101,7 @@ rf_safuel
 ggplot(rf_safuel) + theme_bw()
 
 
-set.seed(100)
+set.seed(154)
 rf_sacomute <- safs(x = x, y = y2,
                   iters = 100,
                   safsControl = rfsacontrl)
@@ -144,13 +119,17 @@ fuelset <- as.formula(
   paste(depfuel,
         paste(opsetfuel, collapse = " + "),
         sep = " ~ "))
-print(fuelset)
-
 regfuel <- lm(fuelset,data=lnreghib)
 
 summary(regfuel)
 
 vif(regfuel)
+
+viffuel <- as.numeric(vif(regfuel))
+dfviffuel <- as.numeric(cbind(viffuel,opsetfuel))
+
+coefiviffuel <- filter(dfviffuel, viffuel>=10)
+coefiviffuel <- as.data.frame(coefiviffuel)
 
 regfuelmtcln <- lm(y_fuel_consumption_per_capita_2010~x_pop_growth_15_00+x_urban_extent_size_2014+
                       x_wghtd_mean_density_rooms_household+x_prop_white_men+x_prop_low_educ+x_prop_age_65_more+
