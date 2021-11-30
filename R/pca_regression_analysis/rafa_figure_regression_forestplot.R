975a80e2cb69eaf27b9cf8ebcbc1994a0d3d21e4
luizpedrocouto <65123020+luizpedrocouto@users.noreply.github.com>
2021-11-29 23:00:39 -0300

    Update rafa_figure_regression_forestplot.R
    - Updating for the urban form model regression for energy
    -  Adequating the sequence of the results reported in both figures
    -  Improving the White x Grey95 colors' lines sequence

--- R/pca_regression_analysis/rafa_figure_regression_forestplot.R
+++ R/pca_regression_analysis/rafa_figure_regression_forestplot.R
@@ -8,45 +8,44 @@ data('mtcars')
 m <- lm(hp ~mpg + cyl + carb , data=mtcars)
 
 # get regression model output as data.frame  ----------------------------------------------------
-df <- broom::tidy(m, conf.int = TRUE, conf.level = 0.95)
 
+model_all$coefficients <- model_all$coefficients[c(1:13)]
+modeldf <- broom::tidy(model_all, conf.int = TRUE, conf.level = 0.95)
 
-setDT(df)
-df[, term := factor(x = term, levels=term)]
-df[, interval := paste0('(', round(conf.low,1), ', ', round(conf.high,1), ')') ]
+setDT(modeldf)
+modeldf[, term := factor(x = term, levels=term)]
+modeldf[, interval := paste0('(', round(conf.low,1), ', ', round(conf.high,1), ')') ]
 
 ### plot  ----------------------------------------------------
 
-df$color <- rep(c("white", "gray95"), nrow(df)/2)
+modeldf$color <- c("white","gray95","white","gray95","white","gray95","white","gray95",
+                   "white","gray95","white","gray95","white")
+modeldf_2 <- modeldf[order(-modeldf$estimate),]
 
+modeldf <- modeldf[order(modeldf$estimate),]
 
+p <- ggplot(modeldf, aes(x = estimate , y = reorder(term, estimate), xmin = conf.low, xmax = conf.high)) +
+  geom_hline(aes(yintercept = reorder(term, estimate), color = color), size = 7) +
+  geom_pointrange(shape = 21, fill = "blue") +
+  geom_vline(xintercept = 0, linetype = 4) +
+  xlab("Elasticity") +
+  ylab("Variables") +
+  theme_classic() +
+  scale_colour_identity() +
+  scale_y_discrete(limits = rev(modeldf_2$term)) +
+  theme(axis.text.y = element_blank(),
+        axis.title.y = element_blank())
 
-p <- ggplot(df, aes(x = estimate , y = reorder(term, estimate), xmin = conf.low, xmax = conf.high)) +
-      geom_hline(aes(yintercept = reorder(term, estimate), color = color), size = 7) +
-      geom_pointrange(shape = 22, fill = "black") +
-      geom_vline(xintercept = 1, linetype = 3) +
-      xlab("Elasticity") +
-      ylab("Variables") +
-      theme_classic() +
-      scale_colour_identity() +
-      # scale_y_discrete(limits = rev(df$term)) +
-      theme(axis.text.y = element_blank(),
-            axis.title.y = element_blank())
+data_table <- ggplot(data = modeldf, aes(y = reorder(term, estimate)) ) +
+  geom_hline(aes(yintercept = term, color = color), size = 7) +
+  geom_text(aes(x = 0, label = reorder(term, estimate)), hjust = 0) +
+  geom_text(aes(x = 5, label = reorder(round(estimate,2), estimate) )) +
+  geom_text(aes(x = 7, label = reorder(interval, estimate) ), hjust = '4') +
+  scale_colour_identity() +
+  theme_void() + 
+  theme(plot.margin = margin(5, 0, 35, 5))
 
-
-
-
-data_table <- ggplot(data = df, aes(y = reorder(term, estimate)) ) +
-              geom_hline(aes(yintercept = term, color = color), size = 7) +
-              geom_text(aes(x = 0, label = reorder(term, estimate)), hjust = 0) +
-              geom_text(aes(x = 5, label = reorder(round(estimate,2), estimate) )) +
-              geom_text(aes(x = 7, label = reorder(interval, estimate) ), hjust = 1) +
-              scale_colour_identity() +
-              theme_void() +
-              theme(plot.margin = margin(5, 0, 35, 5)) # !!!!!!!!
-
-
-temp_figure <- gridExtra::grid.arrange(data_table, p, ncol = 2)
+temp_figure <- gridExtra::grid.arrange(data_table,p,layout_matrix = rbind(c(1,4)),ncol = 2)
 
 ggsave(temp_figure, file='forestplot.png', dpi = 300,
        width = 16, height = 10, units = 'cm')
