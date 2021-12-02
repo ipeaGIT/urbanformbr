78bd180fca764b1d4497b2bd4f824b5faa02b9dc
Marcus Saraiva <marcus.saraiva@gmail.com>
2021-12-02 18:04:12 -0300

    Atualização das medidas de crescimento urbano e de ecologia de paisagem

--- R/landscape_metrics/04_metrics_variation_v2.R
+++ R/landscape_metrics/04_metrics_variation_v2.R
@@ -30,29 +30,29 @@ regression_df %>%
   scale_y_log10()
 
 regression_df %>%
-  ggplot(aes(x=x_pop_2015, y=y_wghtd_mean_commute_time)) +
+  ggplot(aes(x=x_pop_2010, y=y_wghtd_mean_commute_time)) +
   geom_point() +
   scale_x_log10() +
   scale_y_log10()
 
 regression_df %>%
-  select(x_wghtd_mean_household_income_per_capita, x_urban_extent_size_2014, x_pop_2015,
-         y_wghtd_mean_commute_time, y_fuel_consumption_per_capita_2010) %>%
+  select(x_wghtd_mean_household_income_per_capita, x_urban_extent_size_2014, x_pop_2010,
+         y_wghtd_mean_commute_time, y_energy_per_capita) %>%
   cor() %>%
   View()
 
 regression_df <- regression_df %>%
-  mutate(pop_log = log(x_pop_2015),
+  mutate(pop_log = log(x_pop_2010),
          urbanextent_log = log(x_urban_extent_size_2014),
          commute_log = log(y_wghtd_mean_commute_time),
-         fuel_log = log(y_fuel_consumption_per_capita_2010),
+         fuel_log = log(y_energy_per_capita),
          income_log = log(x_wghtd_mean_household_income_per_capita))
 
-lm(y_fuel_consumption_per_capita_2010 ~ x_wghtd_mean_household_income_per_capita + x_prop_autos_dom, regression_df) %>% summary()
+lm(y_energy_per_capita ~ x_wghtd_mean_household_income_per_capita + x_prop_autos_dom, regression_df) %>% summary()
 lm(fuel_log ~ income_log + x_prop_autos_dom, regression_df) %>% summary()
 
 
-lm(y_wghtd_mean_commute_time ~ x_pop_2015 * urbanextent_log, regression_df) %>% summary()
+lm(y_wghtd_mean_commute_time ~ x_pop_2010 * urbanextent_log, regression_df) %>% summary()
 lm(commute_log ~ pop_log + urbanextent_log, regression_df) %>% summary()
 lm(y_wghtd_mean_commute_time ~ pop_log * urbanextent_log, regression_df) %>% summary()
 
@@ -82,14 +82,14 @@ summary(urbanform_reg)
 
 metrics_df %>%
   filter(year == "2014") %>%
-  ggplot(aes(y=density05km, x = avg_cell_distance, color = proportion_largest_patch, size = population)) +
+  ggplot(aes(y=density01km, x = compacity, color = proportion_largest_patch, size = population)) +
   # geom_path(aes(x=density10km, y = avg_cell_distance, group = code_muni)) +
   geom_point() +
   scale_x_log10() +
   scale_y_log10() +
   scale_color_distiller(palette = "Spectral", direction = 1) +
-  geom_hline(yintercept = 461.19) +
-  geom_vline(xintercept = 5.629) +
+  # geom_hline(yintercept = 461.19) +
+  # geom_vline(xintercept = 5.629) +
   labs(title = "Densidade vs Espraiamento, Fragmentação e População") +
   theme_light()
 
@@ -276,7 +276,7 @@ plot_vars_group <- function(data, var1, var2) {
     # facet_wrap( ~ group)
 }
 
-plot_vars_group(metrics_df, "avg_cell_distance", "density05km")
+plot_vars_group(metrics_df, "compacity", "density01km")
 
 
 
---
+++ R/urban_growth/02.1_prepare_data.R
@@ -0,0 +1,42 @@
+#' Script para preparar plots para o relatório
+
+source('R/setup.R')
+source("R/colours.R")
+library("ggtern")
+library("PupillometryR")
+mapviewOptions(platform = "leaflet")
+
+
+# spatial data - cells classified by growth type
+urban_extent_processed <- read_rds(file = "../../data/urbanformbr/urban_growth/grid_uca_growth.rds") %>%
+  mutate(growth_type = if_else(status == "consolidated", "upward", status)) %>%
+  mutate(growth_type = factor(growth_type,
+                              levels = c("upward", "infill", "extension", "leapfrog")))
+
+# taxas de crescimento por cidade, período e tipo de crescimento
+urban_growth_df <- read_rds("../../data/urbanformbr/urban_growth/urban_growth.rds") %>%
+  ungroup() %>%
+  mutate(growth_type = factor(growth_type,
+                              levels = c("upward", "infill", "extension", "leapfrog")))
+
+
+# porte da cidade
+city_size_df <- urban_growth_df %>%
+  ungroup() %>%
+  filter(period_start == 2000, period_end == 2014) %>%
+  select(code_muni, name_uca_case, pop = pop_end, built = built_end) %>%
+  distinct() %>%
+  mutate(size = if_else(pop >= 1000000, "grande", "médio")) %>%
+  select(code_muni, name_uca_case, size)
+
+urban_growth_df <- left_join(urban_growth_df, city_size_df,
+                             by = c("code_muni", "name_uca_case"))
+
+# sinaliza observações como outliers
+urban_growth_df <- urban_growth_df %>%
+  group_by(period_start, period_end) %>%
+  mutate(is_outlier_pop = (pop_geo_growth <= quantile(pop_geo_growth, 0.02, na.rm = T) |
+                             pop_geo_growth >= quantile(pop_geo_growth, 0.95, na.rm = T)),
+         is_outlier_built = (built_geo_growth <= quantile(built_geo_growth, 0.02, na.rm = T) |
+                               built_geo_growth >= quantile(built_geo_growth, 0.95, na.rm = T)))
+
