ddb2762163dd44204a7054c486618d169cac0137
Joaobazzo <joao.bazzo@gmail.com>
2021-12-07 21:48:32 -0300

    adjust script energy

--- R/fuel_ANP/01_download_ANP.R
+++ R/fuel_ANP/01_download_ANP.R
@@ -6,98 +6,25 @@ library(data.table)
 
 #### 1. Donwload raw data ---------------------------------------------------------------
 
+# main link
+#
+# https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/vendas-de-derivados-de-petroleo-e-biocombustiveis
 
-fixed_link <- "https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-estatisticos/de/arquivos-vendas-de-derivados-de-petroleo-e-biocombustiveis/"
-fixed_link1 <- "http://www.anp.gov.br/arquivos/dados-estatisticos/vendas-combustiveis/"
+link_gasoline <- "https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/vdpb/vaehdpm/gasolina-c/vendas-anuais-de-gasolina-c-por-municipio.csv"
+link_etanol <- "https://www.gov.br/anp/pt-br/centrais-de-conteudo/dados-abertos/arquivos/vdpb/vaehdpm/etanol-hidratado/vendas-anuais-de-etanol-hidratado-por-municipio.csv"
+
 `%nin%` <- Negate(`%in%`)
 `%nlike%` <- Negate(`%like%`)
 
-etanol_link <- c( paste0("etanol-hidratado-municipio-",
-                         c(2000,2017,2018),
-                         ".xlsx"),
-                  paste0("etanol-hidratado-municipio-",
-                         c(2001:2016),
-                         ".xls"))
-
-gasolina_link <- c(paste0("gasolina-C-municipio-",
-                          c(2000,2010,2017:2018),
-                          ".xlsx"),
-                   paste0("gasolina-C-municipio-",
-                          c(2001:2009,2011:2016),
-                          ".xls"))
-
-diesel_link <- c(paste0("oleo-diesel-municipio-",
-                        c(2000,2017,2018),
-                        ".xlsx"),
-                 paste0("oleo-diesel-municipio-",
-                        c(2001:2016),
-                        ".xls"))
-
-fuel_link <- c(etanol_link,gasolina_link,diesel_link)
-fuel_link <- fuel_link[fuel_link %like% "2010"]
-
-for(i in 1:length(fuel_link)){ # i = 1
-
-  # message
-  message(fuel_link[i])
-
-  # destination file
-  tmp_destifile <- gsub(pattern = "/"
-                        ,replacement = ""
-                        ,x = fuel_link[i])
-  # download
-  if(fuel_link[i] %like% "2019"){
-    download.file(url = paste0(fixed_link,fuel_link[i]),
-                  destfile = paste0("../../data-raw/ANP/",tmp_destifile),mode = "wb")
-  }else{
-    download.file(url = paste0(fixed_link1,fuel_link[i]),
-                  destfile = paste0("../../data-raw/ANP/",tmp_destifile),mode = "wb")
-  }
+download.file(url = link_gasoline,
+              destfile = "../../data-raw/ANP/vendas-anuais-de-gasolina-c-por-municipio.csv"
+              ,mode = "wb")
+download.file(url = link_etanol,
+              destfile = "../../data-raw/ANP/vendas-anuais-de-etanol-hidratado-por-municipio.csv"
+              ,mode = "wb")
 
 
-}
+df_etanol_raw <- fread("../../data-raw/ANP/vendas-anuais-de-gasolina-c-por-municipio.csv",encoding = "UTF-8")
 
-
-
-#### 2. Pile up data of all years and types of fuel ---------------------------------------------------------------
-
-xlsx_files <- list.files(path = "../../data-raw/ANP/",pattern = ".xlsx",full.names = TRUE)
-xslx_all <- lapply(seq_along(xlsx_files),function(i){ # i = 1
-  message(i)
-  dt <- openxlsx::read.xlsx(xlsxFile = xlsx_files[i],startRow = 5) %>% data.table::setDT()
-  dt <- openxlsx::read.xlsx(xlsxFile = "../../data-raw/ANP/etanol-hidratado-municipio-2000.xlsx",startRow = 5) %>% data.table::setDT()
-  colnames(dt) <- c("cod_ibge","municipios","volume")
-  dt <- dt[!is.na(cod_ibge) & !is.na(municipios) & !is.na(volume)]
-  # add fuel
-  dt[,fuel := data.table::fcase(xlsx_files[i] %like% "etanol","etanol",
-                                xlsx_files[i] %like% "gasolina","gasolina",
-                                xlsx_files[i] %like% "diesel","diesel")]
-  # extract year
-  dt[,year := as.numeric(gsub("[^\\d]+", "", xlsx_files[i], perl=TRUE))]
-  return(dt)
-}) %>% data.table::rbindlist()
-
-# XLS
-xls_files <- list.files(path = "../../data-raw/ANP/",pattern = ".xls",full.names = TRUE)
-xls_files <- xls_files[xls_files %nlike% "xlsx"]
-xls_all <- lapply(seq_along(xlsx_files),function(i){ # i = 1
-  message(i)
-  dt <- readxl::read_xls(path = xls_files[i],skip = 5) %>% data.table::setDT()
-  colnames(dt) <- c("cod_ibge","municipios","volume")
-  dt <- dt[!is.na(cod_ibge) & !is.na(municipios) & !is.na(volume)]
-
-  # add fuel
-  dt[,fuel := data.table::fcase(xls_files[i] %like% "etanol","etanol",
-                                xls_files[i] %like% "gasolina","gasolina",
-                                xls_files[i] %like% "diesel","diesel")]
-  # extract year
-  dt[,year := as.numeric(gsub("[^\\d]+", "", xls_files[i], perl=TRUE))]
-  return(dt)
-}) %>% data.table::rbindlist()
-
-# merge
-dt_all <- rbind(xls_all,xslx_all)
-
-
-# save data
-readr::write_rds(x = dt_all,file = "../../data/urbanformbr/fuel/fuel_cities.rds",compress = "gz")
+names(df_etanol_raw) <- janitor::make_clean_names(names(df_etanol_raw))
+df_etanol_raw
--- R/fuel_ANP/02_estimate_energy.R
+++ R/fuel_ANP/02_estimate_energy.R
@@ -14,10 +14,8 @@ br <- geobr::read_municipality() %>%
   data.table::setDT() %>%
   .[,code_muni := as.character(code_muni)]
 
-anp <- readr::read_rds("data/fuel_cities.rds") %>%
-  .[fuel != "diesel",] %>%
-  .[year == 2010,] %>%
-  .[,cod_ibge := as.character(cod_ibge)]
+df_etanol_raw <- data.table::fread("../../data-raw/ANP/vendas-anuais-de-etanol-hidratado-por-municipio.csv",encoding = "UTF-8")
+df_gasolina_raw <- data.table::fread("../../data-raw/ANP/vendas-anuais-de-gasolina-c-por-municipio.csv",encoding = "UTF-8")
 
 tmp_pop <- sidrar::get_sidra(x = 202
                          , variable = 93
@@ -34,11 +32,21 @@ pop <- data.table::copy(tmp_pop) %>%
 
 names(pop) <- c("cod_ibge","pop")
 
+# organize ANP data
+
+names(df_etanol_raw) <- janitor::make_clean_names(names(df_etanol_raw))
+names(df_gasolina_raw) <- janitor::make_clean_names(names(df_gasolina_raw))
+df_etanol_raw[1]
+df_gasolina_raw[1]
+anp <- rbind(df_gasolina_raw,df_etanol_raw)
+
 # 3) processing -------
 
-anp2 <- data.table::dcast(data = anp
-                          , formula =  cod_ibge + municipios ~ fuel
-                          , value.var = "volume")
+anp2 <- data.table::dcast(data = anp[ano == 2010]
+                          , formula =  codigo_ibge + municipio ~ produto
+                          , value.var = "vendas")
+names(anp2) <- janitor::make_clean_names(names(anp2))
+anp2[,codigo_ibge := as.character(codigo_ibge)]
 
 # TEP ---------
 p_etanol <- units::set_units(809,"kg/m^3")
@@ -46,18 +54,18 @@ p_gasolina <- units::set_units(742,"kg/m^3")
 pci_etanol <- units::set_units(6300,"kcal/kg")
 pci_gasolina <- units::set_units(10400,"kcal/kg")
 
-anp2[,gasolina := as.numeric(gasolina)]
+anp2[,gasolina_c := as.numeric(gasolina_c)]
 anp2[,etanol := as.numeric(etanol)]
 
 anp2[is.na(etanol), etanol := 0]
-anp2[is.na(gasolina), gasolina := 0]
+anp2[is.na(gasolina_c), gasolina_c := 0]
+
+anp2[,gasolina_c := units::set_units(gasolina_c,"liters") %>% units::set_units("m^3")]
+anp2[,etanol := units::set_units(etanol,"liters") %>% units::set_units("m^3")]
 
 
-anp2[,gasolina := units::set_units(gasolina,"m^3")]
-anp2[,etanol := units::set_units(etanol,"m^3")]
 
-
-anp2[,tep := etanol * p_etanol * pci_etanol + gasolina * p_gasolina * pci_gasolina]
+anp2[,tep := etanol * p_etanol * pci_etanol + gasolina_c * p_gasolina * pci_gasolina]
 
 units::install_unit(symbol = "toe"
                     , def = "41 868 GJ"
@@ -67,17 +75,17 @@ anp2[,tep := units::set_units(tep,"toe")]
 
 
 # add uca info
-anp2[uca, on = c("cod_ibge" = "code_muni")
+anp2[uca, on = c("codigo_ibge" = "code_muni")
      , code_urban_concentration := i.code_urban_concentration]
 
 anp2 <- anp2[!is.na(code_urban_concentration),]
 
 
 # add pop info
-anp2[pop,on = c("cod_ibge"="cod_ibge"),pop_2010 := i.pop]
+anp2[pop,on = c("codigo_ibge"="cod_ibge"),pop_2010 := i.pop]
 
 # sum values
-varCols <- c("etanol","gasolina","tep","pop_2010")
+varCols <- c("etanol","gasolina_c","tep","pop_2010")
 anp3 <- data.table::copy(anp2)[,
                                lapply(.SD,sum,na.rm = TRUE)
                                , .SDcols = varCols
@@ -93,3 +101,4 @@ anp3 <- anp3[,.SD,.SDcols = c("code_urban_concentration","tep")]
 
 # 4) save-----
 readr::write_rds(anp3,"../../data/urbanformbr/pca_regression_df/energy_per_capita_2010.rds")
+
