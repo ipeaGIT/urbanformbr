47e90268fea9a923d1241f01320a1b4473825587
João Pedro Figueira Amorim Parga <parga.joao@gmail.com>
2022-05-05 19:28:58 -0300

    Update urban expansion evolution figure and save it

--- R/figures_code/100_urban_extent_evolution_1990_2014.R
+++ R/figures_code/100_urban_extent_evolution_1990_2014.R
@@ -25,13 +25,20 @@ source("R/fun_support/colours.R")
 #salvador: 2927408
 #teresina: 2211001
 
+nomes <- c(
+  "Belém/PA"
+  , "Belo Horizonte/MG"
+  , "Salvador/BA"
+  , "Teresina/PI"
+)
+
 codigos <- c(
-  1501402
-  , 3106200
-  , 5002704
-  , 4305108
+  #1501402
+  3106200
+  #, 5002704
+  #, 4305108
   , 2927408
-  , 2211001
+  #, 2211001
 )
 
 anos <- c(1990,2000,2014)
@@ -77,66 +84,108 @@ df_shapes <- readr::read_rds("../../data/urbanformbr/urban_area_shapes/urban_are
 df_shapes <- subset(df_shapes, code_urban_concentration %in% codigos)
 
 
+
+# reproject ---------------------------------------------------------------
+ucas <- geobr::read_urban_concentrations(simplified = F)
+
+
+df_shapes <- sf::st_transform(df_shapes, crs(ucas))
+df_extents <- sf::st_transform(df_extents, crs(ucas))
+
+# centroids ---------------------------------------------------------------
+
+df_shapes <- df_shapes %>%
+  mutate(centroid = sf::st_centroid(df_shapes) %>% st_geometry())
+
+padding <- 0.55
+
 # plot --------------------------------------------------------------------
-a <- df_shapes %>%
-  dplyr::group_by(name_uca_case) %>%
-  tidyr::nest() %>%
-  dplyr::mutate(
-    plot = purrr::map2(
-      .x = data, .y = name_uca_case,
-      ~ggplot(data = .x) +
-        geom_sf() +
-        aop_style() +
-        theme(
-          axis.line.x = element_blank(),
-          axis.text = element_blank()
-        ) +
-        labs(
-          title = .y
-        )
-    )
-  )
+df_shapes <- df_shapes %>%
+  dplyr::arrange(name_urban_concentration)
 
+f_plot <- function(code){
 
-f_teste <- function(name){
+  # code <- 3106200
+  extents_s <- subset(df_extents, code_urban_concentration == code)
 
-  # name <- "belo_horizonte_mg"
-  extents_s <- subset(df_extents, name_uca_case == name)
+  shapes_s <- subset(df_shapes, code_urban_concentration == code)
 
-  shapes_s <- subset(df_shapes, name_uca_case == name)
+  ucas_s <- subset(ucas, code_urban_concentration == code)
 
   ggplot() +
     geom_sf(
       data = shapes_s
       ,aes()
+      , fill = "light grey"
+      , colour = "darkgray"
+      , size = 0.75
+    ) +
+    geom_sf(
+      data = ucas_s
+      ,aes()
+      , fill = "light grey"
+      , colour = "grey"
     ) +
     geom_sf(
       data = extents_s
       ,aes(fill = forcats::fct_rev(factor(year)))
+      , colour = NA
     ) +
-    scale_fill_viridis_d() +
-    aop_style() +
+    coord_sf(
+      xlim = c(
+        shapes_s$centroid[[1]][1] - padding,
+        shapes_s$centroid[[1]][1] + padding
+        )
+      ,ylim = c(
+        shapes_s$centroid[[1]][2] - padding,
+        shapes_s$centroid[[1]][2] + padding
+      )
+      ,expand = F
+    ) +
+    #scale_fill_manual(values = )
+    scale_fill_viridis_d(option = "inferno",direction = -1) +
+    theme_map() +
+    #aop_style() +
     theme(
-      axis.line.x = element_blank(),
-      axis.text = element_blank(),
-      legend.position = "bottom"
+      axis.line.x = element_blank()
+      ,axis.text = element_blank()
+      ,legend.position = "bottom"
+      ,panel.border = element_rect(fill = NA, color = "grey80")
+      ,legend.text = element_text(size = 10)
+      ,legend.title = element_blank()
+      #,panel.spacing.y = unit(-.5, "cm")
+      #,plot.margin = unit(c(0.25,0.25,0.25,0.25), "cm")
+      #,text = element_text(family = "Helvetica", colour = "#808080", size = 14),
+
     ) +
     labs(
       subtitle = shapes_s$name_urban_concentration
       , fill = "Ano"
     ) +
-    guides(fill = guide_legend(reverse = T))
+    guides(fill = guide_legend(reverse = T,
+                               override.aes = list(colour = "black")
+                               ))
 
 }
 
 gg_out <- purrr::map(
-  .x = df_shapes$name_uca_case,
-  ~f_teste(name = .x)
+  .x = df_shapes$code_urban_concentration,
+  ~f_plot(code = .x)
 )
 
-6666666
 a <- purrr::reduce(gg_out, .f = `+`)
+
+#png(here::here("figures", "teste.png"),
+#    width = 16, height = 16, units = "cm", res = 300, type = "cairo"
+#)
+
 a + patchwork::plot_layout(guides = "collect") & theme(legend.position = "bottom")
 
+#dev.off
+
 # save plot ---------------------------------------------------------------
+ggsave(filename = here::here("figures", "urban_extent_evolution.png"),
+       width = 16, height = 11, units = "cm", dpi = 300, device = "png")
+
+
 
