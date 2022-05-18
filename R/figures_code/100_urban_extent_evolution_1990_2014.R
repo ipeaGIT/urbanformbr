07edb249b60fb7f9c7d35ee9a2143e5fd9389e59
João Pedro Figueira Amorim Parga <parga.joao@gmail.com>
2022-05-18 18:14:08 -0300

    Update urban evolution plot - part 4 - create buffers to make plots same scale and update colours

--- R/figures_code/100_urban_extent_evolution_1990_2014.R
+++ R/figures_code/100_urban_extent_evolution_1990_2014.R
@@ -85,9 +85,9 @@ df_extents <- df_extents %>%
 df_extents <- df_extents %>%
   dplyr::mutate(
     hex = data.table::fcase(
-      year == 1990, "#000004FF"
-      , year == 2000, "#BB3754FF"
-      , year == 2014, "#FCFFA4FF"
+      year == 1990, "#edf8b1" #"#000004FF"
+      , year == 2000, "#7fcdbb" #"#BB3754FF"
+      , year == 2014, "#2c7fb8" #FCFFA4FF"
       , default = NA
     )
   )
@@ -102,64 +102,73 @@ df_shapes <- subset(df_shapes, code_urban_concentration %in% codigos)
 
 # * ucas (non-dissolved) ----------------------------------------------------
 
-ucas <- geobr::read_urban_concentrations(simplified = F)
+#ucas <- geobr::read_urban_concentrations(simplified = F)
 
 
 # function tiles ----------------------------------------------------------
 
-f_tiles <- function(code_uca){
-
-  # code_uca <- 2927408
-  # subset uca
-  df_shapes_s <- subset(df_shapes, code_urban_concentration == code_uca)
-
-  # download tile
-  base_tile <- maptiles::get_tiles(
-    x = df_shapes_s
-    , crop = T
-    , provider = "CartoDB.PositronNoLabels"
-    , zoom = 11
-  )
-
-  # display map
-  #maptiles::plot_tiles(base_tile)
-
-  # convert tile to df
-  df_tile <- terra::as.data.frame(
-    x = base_tile, xy = T
-  ) %>%
-    dplyr::mutate(
-      hex = rgb(red, green, blue, maxColorValue = 255),
-      code_urban_concentration = code_uca
-    ) %>%
-    dplyr::select(-c(red, green, blue))
-
-  return(df_tile)
-
-}
+# f_tiles <- function(code_uca){
+#
+#   # code_uca <- 2927408
+#   # subset uca
+#   df_shapes_s <- subset(df_shapes, code_urban_concentration == code_uca)
+#
+#   # download tile
+#   base_tile <- maptiles::get_tiles(
+#     x = df_shapes_s
+#     , crop = T
+#     , provider = "CartoDB.PositronNoLabels"
+#     , zoom = 11
+#   )
+#
+#   # display map
+#   #maptiles::plot_tiles(base_tile)
+#
+#   # convert tile to df
+#   df_tile <- terra::as.data.frame(
+#     x = base_tile, xy = T
+#   ) %>%
+#     dplyr::mutate(
+#       hex = rgb(red, green, blue, maxColorValue = 255),
+#       code_urban_concentration = code_uca
+#     ) %>%
+#     dplyr::select(-c(red, green, blue))
+#
+#   return(df_tile)
+#
+# }
 
 # * run f_tiles -----------------------------------------------------------
 
-df_tiles <- purrr::map(
-  .x = codigos,
-  ~f_tiles(code_uca = .x)
-)
+#df_tiles <- purrr::map(
+#  .x = codigos,
+#  ~f_tiles(code_uca = .x)
+#)
 
-names(df_tiles) <- paste0("df_",codigos)
+#names(df_tiles) <- paste0("df_",codigos)
 
 
 # reproject ---------------------------------------------------------------
 
-df_shapes <- sf::st_transform(df_shapes, crs(ucas))
-df_extents <- sf::st_transform(df_extents, crs(ucas))
+#df_shapes <- sf::st_transform(df_shapes, crs(ucas))
+#df_extents <- sf::st_transform(df_extents, crs(ucas))
 
-# centroids ---------------------------------------------------------------
+df_shapes <- sf::st_transform(x = df_shapes, crs = 4326)
+df_extents <- sf::st_transform(x = df_extents, crs = 4326)
+#ucas <- sf::st_transform(x = ucas, crs = 4326)
+
+
+# centroid and buffer -----------------------------------------------------
 
 df_shapes <- df_shapes %>%
   mutate(centroid = sf::st_centroid(df_shapes) %>% st_geometry())
 
+buffer_dist <- 50000
+units(buffer_dist) <- "m"
+
+
 # plot --------------------------------------------------------------------
-padding <- 0.55
+#padding <- 0.55
 
 df_shapes <- df_shapes %>%
   dplyr::arrange(name_urban_concentration)
@@ -171,15 +180,21 @@ f_plot <- function(code_uca){
 
   shapes_s <- subset(df_shapes, code_urban_concentration == code_uca)
 
-  ucas_s <- subset(ucas, code_urban_concentration == code_uca)
+  #ucas_s <- subset(ucas, code_urban_concentration == code_uca)
 
-  df_tiles_s <- pluck(df_tiles, grep(as.character(code_uca), names(df_tiles)))
+  #df_tiles_s <- pluck(df_tiles, grep(as.character(code_uca), names(df_tiles)))
 
+  # get buffer
+  buffer_s <- sf::st_buffer(shapes_s$centroid, dist = buffer_dist)
+  b_box_s <- sf::st_bbox(buffer_s)
+
+
+  # plot
   ggplot() +
     # types of map tile: rosm::osm.types
     ggspatial::annotation_map_tile(
       zoom = 12
-      #, zoomin = 10
+      #, zoomin = -1
       , type = "cartolight" #cartodark
     ) +
     geom_sf(
@@ -199,7 +214,16 @@ f_plot <- function(code_uca){
       , height = unit(1, "cm")
       , width = unit(1, "cm")
       ) +
-    scale_fill_viridis_d(option = "inferno", direction = 1) +
+    ggspatial::shadow_spatial(b_box_s) +
+    scale_fill_manual(
+      values = c(
+        "1990" = "#edf8b1"  #
+        , "2000" = "#41b6c4" #
+        , "2014" = "#253494" #
+          )
+    ) +
+    #scale_fill_brewer(palette = "OrRd", direction = -1) +
+    #scale_fill_viridis_d(option = "inferno", direction = 1) +
     theme_map() +
     theme(
       axis.line.x = element_blank()
@@ -217,7 +241,7 @@ f_plot <- function(code_uca){
       , fill = "Ano"
     ) +
     guides(
-      fill = guide_legend(reverse = T, override.aes = list(colour = "black"))
+      fill = guide_legend(reverse = F, override.aes = list(colour = "black"))
     )
 
 }
@@ -227,23 +251,22 @@ gg_out <- purrr::map(
   ~f_plot(code_uca = .x)
 )
 
+# patchwork::wrap_plots()
 gg_final <- purrr::reduce(gg_out, .f = `+`)
 
 #png(here::here("figures", "teste.png"),
 #    width = 16, height = 16, units = "cm", res = 300, type = "cairo"
 #)
 
-gg_final + patchwork::plot_layout(guides = "collect") & theme(legend.position = "bottom")
+gg_final +
+  patchwork::plot_layout(guides = "collect") &
+  theme(legend.position = "bottom")
 
 #dev.off
 
 
-
-# test ggspatial ----------------------------------------------------------
-
-
 # save plot ---------------------------------------------------------------
-ggsave(filename = here::here("figures", "teste.png"),
+ggsave(filename = here::here("figures", "urban_extenet_evolution.png"),
        width = 16, height = 11, units = "cm", dpi = 300, device = "png")
 
 
